4cea48f3d419fa458d80d9e3738d3b5fbe88b761
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2017-04-05 18:29:12 +0900

    remake "who assign"

--- scripts/python/github-api.py
+++ scripts/python/github-api.py
@@ -10,6 +10,7 @@ if ARGC == 4:
     REPO_PATH = (ARGV[2])
     PULL_NO = int((ARGV[3]))
 else:
+    print(ARGV)
     print("Usage: %s GITHUB_TOKEN" % ARGV[0])
     sys.exit()
 
@@ -19,9 +20,9 @@ repo = g.get_repo(REPO_PATH)
 pull = repo.get_pull(PULL_NO)
 
 if pull.assignee:
-    print(pull.assignee.login + "was assignee in [" + pull.title + "]")
+    print("@%s was assignee in [ %s ]" % (pull.assignee.login, pull.title))
 else:
     contributors = repo.get_contributors()
     asisignee = random.choice(contributors)
-    print(contributors + "is best assignee in [" + pull.title + "]")
+    print("@%s is best assignee in [ %s ]" % (pull.assignee.login, pull.title))
 
